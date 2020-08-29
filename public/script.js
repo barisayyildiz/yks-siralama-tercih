17a1d2d8966fe9c75e6460a0fb1825678301966c
barisayyildiz <barisayyildiz.1799@gmail.com>
2020-08-29 14:08:50 +0300

    AYT tablosu eklendi

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 //console.log(document.querySelector("#tyt-table").rows[1].cells[1]);
 
 let tyt_table = document.querySelector("#tyt-table");
+let ayt_table = document.querySelector("#ayt-table")
 
 document.querySelector("#tyt-table").addEventListener("input", e => {
     
@@ -23,23 +24,25 @@ document.querySelector("#tyt-table").addEventListener("input", e => {
 
 });
 
+document.querySelector("#ayt-table").addEventListener("input", e => {
+    
+    //console.log(e.target.value);
+    //console.log(tyt_table.rows[1].cells[1].childNodes[1].value);
 
-function netHesapla()
-{
-    let tyt_table = document.querySelector("#tyt-table");
-    for(let i=1; i<=3; i++) //columns
+    for(let i=1; i<=4; i++)
     {
-        //console.log(document.querySelector("#tyt-table").rows[i].cells[i]);
-        
-        //row 1, row2 => row'3 e kaydet
-        let d = parseInt(tyt_table.rows[1].cells[i]);
-        let y = parseInt(tyt_table.rows[2].cells[i]);
+        let d = parseInt(ayt_table.rows[1].cells[i].childNodes[1].value);
+        d = isNaN(d) ? 0 : d;
+        let y = parseInt(ayt_table.rows[2].cells[i].childNodes[1].value);
+        y = isNaN(y) ? 0 : y;
         let net = d - y/4;
 
         console.log(d, y, net);
-        
-        tyt_table.rows[3].cells[i].innerHTML = net;
-    }
-}
 
+        ayt_table.rows[3].cells[i].innerHTML = net;
+        ayt_table.rows[3].cells[i].value = net;
+
+    }
+
+});
 
