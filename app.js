08d86067e43f2bc22694eb5128d47460599004c0
barisayyildiz <barisayyildiz.1799@gmail.com>
2020-08-29 22:47:48 +0300

    veritabanı bağlandı 2019 ham puan yığılma tablosu kaydedildi

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require("express");
-const exphbs = require("express-handlebars");
 const app = express();
+
+const exphbs = require("express-handlebars");
+
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/yks', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const Model = mongoose.model('ham_puan', {puan : Number, tyt : Number, say : Number, ea : Number}, 'ham_puan');
+
+
+//Model.create({puan : 600, tyt : 1, say : 1, ea : 1});
+
 
 app.listen(3000, () => {console.log("I'm listening...")});
 
@@ -33,12 +43,15 @@ app.post("/submit", (req, res) => {
     req.body = arrange(req.body);
     req.body = toInt(req.body);
 
-    let tyt_point = calculateTyt(req.body["tyt-tur"], req.body["tyt-sos"], req.body["tyt-mat"], req.body["tyt-fen"]);
+    let tyt_point = calculateTyt(req.body["tyt-tur"], req.body["tyt-sos"], req.body["tyt-mat"], req.body["tyt-fen"], req.body["obp"]);
     let ayt_point = calculateAyt(req.body);
+
+    console.log(req.body["obp"]);
     
     console.log(tyt_point);
     console.log(ayt_point);
 
+    Model.find({}, (err, docs) => console.log(docs));
 
 
     res.render("result", {
@@ -71,9 +84,14 @@ function toInt(data)
     return data;
 }
 
-function calculateTyt(tur, sos, mat, fen)
+function calculateTyt(tur, sos, mat, fen, obp)
 {
-    return (tur * 3.4 + sos * 3.4 + mat * 3.3 + fen * 3.4 + 100);
+    //return (tur * 3.4 + sos * 3.4 + mat * 3.3 + fen * 3.4 + 100);
+    return {
+        hamPuan : tur * 3.4 + sos * 3.4 + mat * 3.3 + fen * 3.4 + 100,
+        yerPuan : tur * 3.4 + sos * 3.4 + mat * 3.3 + fen * 3.4 + 100 + obp * 0.6
+    };
+
 }
 
 function calculateAyt(data)
@@ -81,10 +99,13 @@ function calculateAyt(data)
     let result = {};
 
     result.say = data["tyt-tur"] * 1.32 + data["tyt-sos"] * 1.36 + data["tyt-mat"] * 1.32 + data["tyt-fen"] * 1.36; //tyt
-    result.say += data["ayt-mat"] * 3 + data["ayt-fiz"] * 2.85 + data["ayt-kim"] * 3.07 + data["ayt-bio"] * 3.07 + 100 + data.obp * 0.6 //ayt
+    result.say += data["ayt-mat"] * 3 + data["ayt-fiz"] * 2.85 + data["ayt-kim"] * 3.07 + data["ayt-bio"] * 3.07 + 100; //ayt
     
     result.ea = data["tyt-tur"] * 1.32 + data["tyt-sos"] * 1.36 + data["tyt-mat"] * 1.32 + data["tyt-fen"] * 1.36;
-    result.ea += data["ayt-mat"] * 3 + data["ayt-edeb"] * 3 + data["ayt-tar"] * 2.8 + data["ayt-cog"] * 3.33 + 100 + data.obp * 0.6;
+    result.ea += data["ayt-mat"] * 3 + data["ayt-edeb"] * 3 + data["ayt-tar"] * 2.8 + data["ayt-cog"] * 3.33 + 100;
 
-    return result;
+    return {
+        hamPuan : {say : result.say, ea : result.ea},
+        yerPuan : {say : result.say + data["obp"] * 0.6, ea : result.ea + data["obp"] * 0.6}
+    };
 }
